0321e34b74d29e93f053f1effc77a1c8effa82a9
Prasun Neupane <prasunneupane14@gmail.com>
2026-06-18 20:30:44 +0545

    Day 9: working on first vue project with everything from scratch,created a small component imported in main,js and see the magic

---
+++ vuework/newvue/src/components/FoodItem.vue
@@ -0,0 +1,24 @@
+<template>
+    <div @click="CountClicks">
+        <h2>{{ name }}</h2>
+        <p>{{ message }}</p>
+        <p>You liked this food {{ Count }} times</p>
+    </div>
+</template>
+<script>
+export default{
+    data(){
+        return{
+            name:"Mexical Food",
+            message:"I want to eat mexican food",
+            Count:0
+        }
+    },
+    methods:{
+        CountClicks(){
+            this.Count++
+        }
+    }
+}
+</script>
+<style></style>
--- vuework/newvue/src/main.js
+++ vuework/newvue/src/main.js
@@ -1,13 +1,14 @@
-import './assets/main.css'
+// import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
+import FoodItem from './components/FoodItem.vue'
 import router from './router'
 
 const app = createApp(App)
-
+app.component("FoodItem",FoodItem);
 app.use(createPinia())
 app.use(router)
 
